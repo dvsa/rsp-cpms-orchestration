eb5b7d4ddd602bb8211ac8070dc795b971065dbc
Ammar Haider <ammar.haider@bjss.com>
2018-08-16 10:20:59 +0100

    sets IsGroupPayment to false for single penalty payments

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -36,7 +36,7 @@ const cardPayment = async (paymentObject, callback) => {
 			ReceiptReference: receiptReference,
 			PaymentCode: penaltyReference,
 			VehicleRegistration: vehicleReg,
-			IsGroupPayment: true,
+			IsGroupPayment: false,
 		});
 
 		callback(null, createResponse({ body: transactionData, statusCode: 200 }));
