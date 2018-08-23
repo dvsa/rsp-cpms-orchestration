09c9e417ffe6fefc513a2fe26d6955ab3a4e9ac7
John Morley <john.morley@bjss.com>
2018-08-23 17:43:05 +0100

    Bump `rsp-validation` to `1.5.7` to validate non-card transactions
    * Wrap call to CPMS transaction validation in feature flag for testing

--- src/utils/cpmsGroupPayment.js
+++ src/utils/cpmsGroupPayment.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import Validation from 'rsp-validation';
+import Validation from 'rsp-validation';
 
 import buildGroupTransactionOptions from '../utils/buildGroupTransactionOptions';
 import Constants from '../utils/constants';
@@ -18,12 +18,15 @@ export default (groupTransactionData) => {
 		console.log('created transaction client');
 
 		const transactionOptions = buildGroupTransactionOptions(groupTransactionData);
-		// const validationResult = Validation.cpmsTransactionValidation(transactionOptions);
-		// if (!validationResult.valid) {
-		// 	const errMsg = validationResult.error.message;
-		// 	reject(errMsg);
-		// 	return;
-		// }
+		if (process.env.VALIDATE_CPMS_TRANSACTIONS) {
+			const validationResult = Validation.cpmsTransactionValidation(transactionOptions);
+			if (!validationResult.valid) {
+				const errMsg = validationResult.error.message;
+				console.log(errMsg);
+				reject(errMsg);
+				return;
+			}
+		}
 		console.log(transactionOptions);
 		transactionClient.post(groupTransactionData.endpoint, transactionOptions)
 			.then((transactionResponse) => {
