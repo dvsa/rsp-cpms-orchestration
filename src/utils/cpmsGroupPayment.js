b4aae00045be3fe3ddf18ae493c9bb9716877c62
James Highfield <James.Highfield@bjss.com>
2019-07-10 10:46:57 +0100

    Log group payment sucess

--- src/utils/cpmsGroupPayment.js
+++ src/utils/cpmsGroupPayment.js
@@ -1,39 +1,50 @@
-import axios from 'axios';
-import Validation from 'rsp-validation';
+import axios from "axios";
+import Validation from "rsp-validation";
 
-import buildGroupTransactionOptions from '../utils/buildGroupTransactionOptions';
-import Constants from '../utils/constants';
-import { logAxiosError, logInfo, logError } from './logger';
+import buildGroupTransactionOptions from "../utils/buildGroupTransactionOptions";
+import Constants from "../utils/constants";
+import { logAxiosError, logInfo, logError } from "./logger";
 
-export default (groupTransactionData) => {
-	return new Promise((resolve, reject) => {
+export default groupTransactionData => {
+  return new Promise((resolve, reject) => {
+    const transactionClient = axios.create({
+      baseURL: Constants.cpmsBaseUrl(),
+      headers: {
+        "Content-Type": "application/vnd.dvsa-gov-uk.v2+json",
+        Authorization: `Bearer ${groupTransactionData.auth.access_token}`
+      }
+    });
 
-		const transactionClient = axios.create({
-			baseURL: Constants.cpmsBaseUrl(),
-			headers: {
-				'Content-Type': 'application/vnd.dvsa-gov-uk.v2+json',
-				Authorization: `Bearer ${groupTransactionData.auth.access_token}`,
-			},
-		});
+    const transactionOptions = buildGroupTransactionOptions(
+      groupTransactionData
+    );
+    const validationResult = Validation.cpmsTransactionValidation(
+      transactionOptions
+    );
+    if (!validationResult.valid) {
+      const errMsg = validationResult.error.message;
+      reject(errMsg);
+      return;
+    }
 
-		const transactionOptions = buildGroupTransactionOptions(groupTransactionData);
-		const validationResult = Validation.cpmsTransactionValidation(transactionOptions);
-		if (!validationResult.valid) {
-			const errMsg = validationResult.error.message;
-			reject(errMsg);
-			return;
-		}
-		transactionClient.post(groupTransactionData.endpoint, transactionOptions)
-			.then((transactionResponse) => {
-				if (typeof transactionResponse.data === 'undefined') {
-					logError('CPMSGroupPaymentNoData', { groupTransactionData });
-					reject(new Error('Call to CPMS returned no data'));
-				}
-				logInfo('CPMSGroupPaymentSuccess', { groupTransactionData });
-				resolve(transactionResponse.data);
-			}).catch((err) => {
-				logAxiosError('CpmsGroupPayment', 'CPMS', err, { groupTransactionData });
-				throw err;
-			});
-	});
+    transactionClient
+      .post(groupTransactionData.endpoint, transactionOptions)
+      .then(transactionResponse => {
+        if (typeof transactionResponse.data === "undefined") {
+          logError("CPMSGroupPaymentNoData", { groupTransactionData });
+          reject(new Error("Call to CPMS returned no data"));
+        }
+        logInfo("CPMSGroupPaymentSuccess", {
+          endpoint: groupTransactionData.endpoint,
+          transactionOptions
+        });
+        resolve(transactionResponse.data);
+      })
+      .catch(err => {
+        logAxiosError("CpmsGroupPayment", "CPMS", err, {
+          groupTransactionData
+        });
+        throw err;
+      });
+  });
 };
