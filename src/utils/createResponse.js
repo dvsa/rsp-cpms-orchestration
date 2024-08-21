ea521a9016975c4762c1c8d1fd56532bb0565be6
Shaun Hare <shaun.hare@dvsa.gov.uk>
2024-08-21 15:55:05 +0100

    fix: added response debugging

--- src/utils/createResponse.js
+++ src/utils/createResponse.js
@@ -1,10 +1,19 @@
 export default ({ body = {}, statusCode = 200 }) => {
+	let responseBody;
+	try {
+		responseBody = JSON.stringify(body);
+	} catch (error) {
+		console.error('Error stringifying response body:', error);
+		console.log('Original body:', body);
+		responseBody = JSON.stringify({ error: 'Internal Server Error' });
+	}
+
 	const response = {
 		statusCode,
 		headers: {
 			'Access-Control-Allow-Origin': '*', // Required for CORS support to work
 		},
-		body: JSON.stringify(body),
+		body: responseBody,
 	};
 	return response;
 };
